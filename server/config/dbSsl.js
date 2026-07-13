5d62127ec379c337a13c3a225ae66a2d8f9beb43
Rupok Khan <rupok124519@gmail.com>
2026-07-14 01:14:33 +0600

    Add Aiven TLS compatibility for Render

--- server/config/dbSsl.js
+++ server/config/dbSsl.js
@@ -1,6 +1,8 @@
 function getDatabaseSsl() {
   if (process.env.DB_SSL !== "true") return undefined;
 
+  const rejectUnauthorized = process.env.DB_SSL_REJECT_UNAUTHORIZED !== "false";
+
   let ca;
   if (process.env.DB_SSL_CA_BASE64) {
     ca = Buffer.from(process.env.DB_SSL_CA_BASE64, "base64").toString("utf8");
@@ -8,7 +10,7 @@ function getDatabaseSsl() {
     ca = process.env.DB_SSL_CA.replace(/\\n/g, "\n");
   }
 
-  return ca ? { ca, rejectUnauthorized: true } : { rejectUnauthorized: true };
+  return ca ? { ca, rejectUnauthorized } : { rejectUnauthorized };
 }
 
 module.exports = { getDatabaseSsl };
