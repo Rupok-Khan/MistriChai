8b491b8f7b74a1678aea5d9cfab523d414ce4781
Rupok Khan <rupok124519@gmail.com>
2026-07-29 11:52:33 +0600

    Allow idempotency header in CORS

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Authorization", "Content-Type", "X-Request-Id"],
+  allowedHeaders: ["Authorization", "Content-Type", "X-Request-Id", "Idempotency-Key"],
   maxAge: 86400
 }));
 app.use(express.json({ limit: "512kb" }));
